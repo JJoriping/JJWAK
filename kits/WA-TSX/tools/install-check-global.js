4041b135ae0a6179730baf030241e2a7835d9b4d
E12-208-B06E <op@jjo.kr>
2018-04-02 09:34:26 +0900

    Fix the bug about uninstalled module usage

--- kits/WA-TSX/tools/install-check-global.js
+++ kits/WA-TSX/tools/install-check-global.js
@@ -1,7 +1,6 @@
 const FS = require("fs");
 const Path = require("path");
 const ChildProcess = require("child_process");
-const JJLog = require("jj-log").default;
 
 const REQUIREMENTS = [
   "webpack",
@@ -12,15 +11,14 @@ const REQUIREMENTS = [
 const EXEC_PATH = Path.resolve(process.execPath, "../node_modules");
 let data;
 
-JJLog.info("Checking global modules...");
+console.log("Checking global modules...");
 for(const v of FS.readdirSync(EXEC_PATH)){
   const i = REQUIREMENTS.indexOf(v);
 
   if(i !== -1) REQUIREMENTS.splice(i, 1);
 }
 if(REQUIREMENTS.length > 0){
-  JJLog.info("Install global modules...");
+  console.log("Install global modules...");
   ChildProcess.spawnSync("npm", ["install", ...REQUIREMENTS, "-g"]);
 }
-JJLog.success("Check");
 process.exit();
