ff77e78d92706c3ee0fe718e710318e35aa2db38
JJoriping <op@jjo.kr>
2019-04-02 20:05:53 +0900

    Support XHR module

--- kits/WA-TSX/src/back/utils/Route.ts
+++ kits/WA-TSX/src/back/utils/Route.ts
@@ -1,7 +1,13 @@
 import Express = require("express");
+
+import { loadLanguages } from "./Language";
 import { PageBuilder } from "./ReactNest";
 
 export default function(App:Express.Application):void{
   App.get("/", PageBuilder("Index"));
+  App.get("/admin/load-languages", (req, res) => {
+    loadLanguages();
+    res.sendStatus(200);
+  });
   App.use((req, res) => res.sendStatus(404));
 }
