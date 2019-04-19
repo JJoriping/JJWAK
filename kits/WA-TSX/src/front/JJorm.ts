c3854063b7056d5d7f527a186ebe8f26543b1c25
JJoriping <op@jjo.kr>
2019-04-19 12:01:02 +0900

    Detach JRef

--- kits/WA-TSX/src/front/JJorm.ts
+++ kits/WA-TSX/src/front/JJorm.ts
@@ -3,30 +3,6 @@ import React = require("react");
 type ActionReceiverPolytable = {
   [key in keyof JJWAK.ActionReceiverTable]: Array<JJWAK.ActionReceiverTable[key]>
 };
-
-export class JRef<T = HTMLElement> extends Object{
-  private itemSetter?:($:T) => void;
-  public item:T;
-  public items:Table<T>;
-
-  constructor(hashFunction?:($:T) => number|string){
-    super();
-    if(hashFunction){
-      this.itemSetter = $ => this.items[hashFunction($)] = $;
-      this.items = {};
-    }else{
-      this.itemSetter = $ => this.item = $;
-    }
-  }
-  hasOwnProperty(key:string):boolean{
-    // NOTE React.RefObject를 모방하기 위함
-    return key === "current" || super.hasOwnProperty(key);
-  }
-  public set current(value:T){
-    // NOTE unmount되는 경우 추적할 수 없고 items에 남게 된다.
-    value && this.itemSetter(value);
-  }
-}
 export default abstract class JJorm<P = {}, S = {}> extends React.PureComponent<P, S>{
   private static ACTION_RECEIVER_POLYTABLE:ActionReceiverPolytable = {};
   private static flushed = false;
