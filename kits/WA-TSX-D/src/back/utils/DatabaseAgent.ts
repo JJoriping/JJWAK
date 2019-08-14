91b9d2f7ea2a1c797e0d0944c12982889e4bebf0
JJoriping <op@jjo.kr>
2019-08-14 16:39:25 +0900

    Add DatabaseAgent.ts

---
+++ kits/WA-TSX-D/src/back/utils/DatabaseAgent.ts
@@ -0,0 +1,48 @@
+import TypeORM = require("typeorm");
+
+import { ValueTransformer } from "typeorm/decorator/options/ValueTransformer";
+
+import DB from "./Database";
+
+export function Transaction():MethodDecorator{
+  return (target, propKey, descriptor) => {
+    if(!(descriptor.value instanceof Function)) return;
+    const original = descriptor.value;
+
+    descriptor.value = function(this:any, ...args:any[]){
+      const self = this;
+      const tip = args.length - 1;
+
+      if(args[tip] instanceof TypeORM.EntityManager){
+        return original.call(self, ...args);
+      }else{
+        return DB.Manager.transaction(manager => {
+          if(args[tip] === undefined){
+            args[tip] = manager;
+          }
+          return original.call(self, ...args, manager);
+        });
+      }
+    } as any;
+  };
+}
+export class Transformer{
+  private static PARSER_POINT_FROM = /^POINT\((\S+) (\S+)\)$/;
+
+  public static Boolean:ValueTransformer = {
+    from: (v:number) => Boolean(v),
+    to: (v:Boolean) => v ? 1 : 0
+  };
+  public static List:ValueTransformer = {
+    from: (v:string) => v ? v.split(',') : [],
+    to: (v:string[]) => v.join(',')
+  };
+  public static Point:ValueTransformer = {
+    from: (v:string) => {
+      return v.match(Transformer.PARSER_POINT_FROM).slice(1).map(Number);
+    },
+    to: (v:number[]) => {
+      return v && `POINT(${v[0]} ${v[1]})`;
+    }
+  };
+}
